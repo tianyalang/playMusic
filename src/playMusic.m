f142f72420259fde0d88bd0a90f73acb14869cf8
tianyalang <44080909+tianyalang@users.noreply.github.com>
2022-01-31 18:05:25 +0800

    Update playMusic.m

--- src/playMusic.m
+++ src/playMusic.m
@@ -1,12 +1,13 @@
 clear
 
 fs = 44100;  % ²ÉÑùÂÊ£¬MP3 standard
-pu_Canon
-sound(pu, fs);
-
+pu_chunjiqxuqu
+% pu_Canon
 % pu_BeiJiaerHu
 % pu_Moon
 % pu_MiXue
+
+sound(pu, fs);
 
 % audiowrite('mysobei.wav', pu, fs); % output music file
 
